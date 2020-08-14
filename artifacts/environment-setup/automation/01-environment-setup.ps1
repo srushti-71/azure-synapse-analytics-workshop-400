da465179cfc7c420cecefc0b9c423a8680f4dc14
srushti-71 <69667304+srushti-71@users.noreply.github.com>
2020-08-14 12:24:35 +0530

    Update 01-environment-setup.ps1

--- artifacts/environment-setup/automation/01-environment-setup.ps1
+++ artifacts/environment-setup/automation/01-environment-setup.ps1
@@ -17,6 +17,20 @@ if($IsCloudLabs){
 
         . C:\LabFiles\AzureCreds.ps1
 
+        $depId = $deploymentID
+        $initstatus = "Started"
+        $validstatus = "NotValidated"
+
+        $uri = 'https://prod-04.centralus.logic.azure.com:443/workflows/8f1e715486db4e82996e45f86d84edc6/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5fJRgTtLIkSidgMmhFXU_DfubS837o8po0BBvCGuGeA'
+        $bodyMsg = @(
+             @{ "DeploymentId" = "$depId"; 
+              "InitiationStatus" =  "$initstatus"; 
+              "ValidationStatus" = "$validstatus" }
+              )
+       $body = ConvertTo-Json -InputObject $bodyMsg
+       $header = @{ message = "StartedByScript"}
+       $response = Invoke-RestMethod -Method post -Uri $uri -Body $body -Headers $header  -ContentType "application/json"
+       
         $userName = $AzureUserName                # READ FROM FILE
         $password = $AzurePassword                # READ FROM FILE
         $clientId = $TokenGeneratorClientId       # READ FROM FILE
